f4e31bf43141eabf918e4f8634317feb5a869589
Nikhil-ig <77897642+Nikhil-ig@users.noreply.github.com>
2025-01-28 15:00:47 +0530

    Add files via upload

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:spotlight/spotlight.dart';
+import 'package:spotlight_effect/spotlight_effect.dart';
 
 void main() {
   runApp(const MyApp());
